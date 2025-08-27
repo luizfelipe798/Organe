8434a77f219b82d7b0ee4e04484dc171461641e0
luizfelipe798 <luizvignoto@gmail.com>
2025-08-27 19:34:46 -0300

    Programação do arquivo "conexao.php"

--- php/conexao.php
+++ php/conexao.php
@@ -1,5 +1,12 @@
 <?php
-    $nomeUsuario = "root";
-    $nomeBancoDeDados = "gerenciadordependencias";
-    $senhaBancoDeDados = "815674815";
-?>
+$nomeServidor = "localhost";
+$nomeUsuario = "root";
+$senha = "815674815";
+$nomeBD = "geradordependencias";
+
+$conn = new mysqli($nomeServidor, $nomeUsuario, $senha, $nomeBD);
+
+if($conn -> connect_error) 
+{
+    die("Conexão falhou: " . $conn -> connect_error);
+}

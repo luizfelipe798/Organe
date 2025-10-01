31854f0380947cb53a1d81b9ab818912bdd0256d
luizfelipe798 <luizvignoto@gmail.com>
2025-10-01 18:15:34 -0300

    .

--- includes/menu.php
+++ includes/menu.php
@@ -1,6 +1,6 @@
 <menu class="menuPrincipal">
-    <a href="index.php">Início</a>
-    <a href="listar.php">Listar Dependências</a>
+    <a href="index.php"class="linksMenuPrincipal">Início</a>
+    <a href="listar.php" class="linksMenuPrincipal">Listar Dependências</a>
 
     <a href="cadastro.php" class="botoesCadastroLoginMenuPrincipal">Cadastrar-se</a>
     <a href="login.php" class="botoesCadastroLoginMenuPrincipal">Login</a>
